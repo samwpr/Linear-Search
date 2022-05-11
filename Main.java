4c2dc4a4c019350acabc9681c0f3dad254a792f5
Sam Wong <80372920+samwpr@users.noreply.github.com>
2022-05-11 02:20:45 +0000

    -

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+		int[] nameofarray = {1, 3, 4, 5, 6, 8, 4, 6, 2, 0};
+
+		int index = linearSearch(nameofarray, 4);
+
+		if (index != -1){
+			System.out.println("Found at index " + index);
+		} else {
+			System.out.println("Not Found");
+		}
   }
+
+	private static int linearSearch(int[] array, int value){
+
+		for(int i = 0; i < array.length; i++){
+			if(array[i] == value){
+				return i;
+			}
+		}
+		return -1;
+	}	
 }
