83316052d07fac467d72cd4b6a7627870377ccf1
Sam Wong <80372920+samwpr@users.noreply.github.com>
2022-05-12 06:04:13 +0000

    -

--- Main.java
+++ Main.java
@@ -1,23 +1,37 @@
+/*******************************************************
+LINEAR SEARCH 
+Iterating through a dataset one element at a time 
+Time complexity = O(n) 
+The larger the dataset the more time is needed 
+
+Disadvantages: 
+Slow for large datasets 
+
+Advantages: 
+Fast for small to medium datasets 
+Does not need sorting 
+Useful for data structures without random access (LinkedList)
+*/
+
 class Main {
   public static void main(String[] args) {
-		int[] nameofarray = {1, 3, 4, 5, 6, 8, 4, 6, 2, 0};
+		int[] array = {1, 2, 3, 4, 9, 7, 6, 5, 0};
 
-		int index = linearSearch(nameofarray, 4);
+		int index = linearSearch(array, 5);
 
-		if (index != -1){
-			System.out.println("Found at index " + index);
+		if(index != -1){
+			System.out.println("Found at: " + index);
 		} else {
-			System.out.println("Not Found");
+			System.out.print("Not Found");
 		}
-  }
-
-	private static int linearSearch(int[] array, int value){
-
-		for(int i = 0; i < array.length; i++){
-			if(array[i] == value){
-				return i;
-			}
-		}
-		return -1;
+		
 	}	
+	private static int linearSearch(int[] array, int target){
+		for(int i = 0; i < array.length; i++){
+			if(array[i] == target){
+				return i;
+			} 
+		}
+		return -1; 
+	}
 }
